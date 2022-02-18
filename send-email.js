ed86c69903da18a14463d0b53a0c13ccaca6a333
rochafialvin <rochafi.dev@gmail.com>
2022-02-18 13:57:43 +0700

    Create function sentVerification

--- send-email.js
+++ send-email.js
@@ -1,3 +1,7 @@
 const sentEmail = (recipient, sender) => {
   console.log(`Email has been sent to ${recipient} from ${sender}`);
 };
+
+const sentVerification = (recipient, token) => {
+  console.log(`Token : ${token}`);
+};
